ec294037ba76c2820c003dab9932ab00bc459b63
Yanioconjota <janioisacura@gmail.com>
2016-08-22 16:42:48 -0300

    Functions problem set updated

--- public/js/functionsproblemset.js
+++ public/js/functionsproblemset.js
@@ -1,19 +1,53 @@
 
-function isEven(n){
-  if(n % 2 === 0){
-    return true;
-  }
+function isEven(num){
+  // if(num % 2 === 0){
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  return num % 2 === 0;
 };
 
-function factorial(f){
-  if(f === 0){
+function factorial(num){
+  if(num === 0){
     return 1
   }
-  return f * factorial(f - 1);
+  return num * factorial(num - 1);
 };
 
-function kebabToSnake(s){
-  var kebab = s;
-  var snake = kebab.replace('-','_');
+function factorialAsc(num){
+  //define a result variable
+  var result = 1;
+  //calculate factorial and store value in result
+  for(var i = 2; i <= num; i++){
+    result *= i;
+  }
+  //return the result variable
+  return result;
+
+  // if(num === 0){
+  //   return 1
+  // }
+  // return num * factorial(num - 1);
+};
+
+function factorialDes(num){
+  if(num === 0){
+    return 1;
+  }
+  //define a resul variable
+  var result = num;
+  //calculate factorial and store value in result
+  for(var i = num-1; i >= 1; i--){
+    result *= i;
+  }
+  //return the result variable
+  return result;
+};
+
+
+function kebabToSnake(str){
+  var kebab = str;
+  var snake = kebab.replace(/-/g,'_');
   return snake;
 };
