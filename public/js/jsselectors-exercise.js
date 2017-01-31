c8b73f58916d502b60c1d22ca2d0be264d00491b
Janio Isacura <janioisacura@gmail.com>
2017-01-31 15:57:44 -0300

    JS Selector exercise completed

---
+++ public/js/jsselectors-exercise.js
@@ -0,0 +1,11 @@
+var tag = document.getElementById("first");
+console.log(tag);
+
+var tag = document.getElementsByTagName("li");
+console.log(tag[0]);
+
+var tag = document.getElementsByClassName("special");
+console.log(tag[0]);
+
+var tag = document.querySelector(".special");
+console.log(tag);
